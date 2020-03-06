037d1ddf038d7090fe06c505abe97a375e03ed04
wangxing <wuyu1562766289@live.com>
2020-03-06 15:53:04 +0800

    project

---
+++ vue/project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <FormTest></FormTest>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import FormTest from '@/components/form';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    FormTest
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/project/src/components/form/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <WForm :model="model" :rules="rules" ref="loginForm">
+      <WFormItem label="用户名" prop="username">
+        <WInput v-model="model.username"></WInput>
+      </WFormItem>
+      <WFormItem label="密码" prop="password">
+        <WInput v-model="model.password" type="password"></WInput>
+      </WFormItem>
+      <WFormItem>
+        <button @click="onLogin">登录</button>
+      </WFormItem>
+    </WForm>
+    {{model}}
+  </div>
+</template>
+
+<script>
+import WInput from "./WInput.vue";
+import WFormItem from "./WFormItem.vue";
+import WForm from "./WForm.vue";
+
+export default {
+  components: {
+    WInput,
+    WFormItem,
+    WForm
+  },
+  data() {
+    return {
+      model: {
+        username: "wx",
+        password: ""
+      },
+      rules: {
+        username: [{ required: true, message: "用户名必填" }],
+        password: [{ required: true, message: "密码必填" }]
+      }
+    };
+  },
+  methods: {
+    onLogin() {
+      this.$refs.loginForm.validate(isvalid => {
+        if(isvalid) {
+          alert('登录！！！');
+        } else {
+          alert('有错！！！');
+        }
+      })
+    }
+  },
+};
+</script>
+
+<style scoped>
+</style>
