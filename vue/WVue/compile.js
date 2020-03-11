dbf9d3efa506c2d897e941b4a3cd62462c9af59f
wangxing <wuyu1562766289@live.com>
2020-03-11 17:25:57 +0800

    vue乞丐版，添加v-model

--- vue/WVue/compile.js
+++ vue/WVue/compile.js
@@ -17,7 +17,7 @@ class Compile {
     const fragment = document.createDocumentFragment();
     // 移动dom节点
     let child;
-    while(child = el.firstChild) {
+    while (child = el.firstChild) {
       fragment.appendChild(child);
     }
 
@@ -28,16 +28,16 @@ class Compile {
     const childNodes = el.childNodes;
 
     Array.from(childNodes).forEach(node => {
-      if(node.nodeType == 1) {
+      if (node.nodeType == 1) {
         // 元素
         this.compileElement(node);
-      } else if(node.nodeType == 3 && /\{\{(.*)\}\}/.test(node.textContent)) {
+      } else if (node.nodeType == 3 && /\{\{(.*)\}\}/.test(node.textContent)) {
         // 只关心{{xxx}}
         this.compileText(node);
       }
 
       // 递归子节点
-      if(node.children && node.childNodes.length > 0) {
+      if (node.children && node.childNodes.length > 0) {
         this.compile(node);
       }
     })
@@ -50,11 +50,14 @@ class Compile {
       const attrName = attr.name;
       const value = attr.value;
 
-      if(attrName.indexOf('w-') == 0) {
+      if (attrName.indexOf('w-') == 0) {
         // 指令
         const dest = attrName.substring(2);
         // 执行更新
         this[dest] && this[dest](node, value);
+      } else if (attrName.indexOf('@') == 0) {
+        const eventName = attrName.substring(1);
+        this.eventHandler(node, value, eventName);
       }
     })
   }
@@ -65,22 +68,44 @@ class Compile {
   html(node, value) {
     this.update(node, value, 'html');
   }
+  model(node, value) {
+    // 数据变了改界面
+    this.update(node, value, "model");
+    // 界面变了改数值
+    node.addEventListener('input', e => {
+      this.$vm[value] = e.target.value;
+    })
+  }
 
   update(node, value, type) {
     const updator = this[type + "Updator"];
     updator && updator(node, this.$vm[value]);
+
+    new Watcher(this.$vm, value, function(val) {
+      updator && updator(node, val);
+    });
   }
   textUpdator(node, value) {
     node.textContent = value;
-    console.log(value);    
+    console.log(value);
   }
   htmlUpdator(node, value) {
     node.innerHTML = value;
   }
-
+  modelUpdator(node, value) {
+    node.value = value;
+  }
   compileText(node) {
     // 表达式
     const value = RegExp.$1;
     this.update(node, value, "text");
+  }
+
+  eventHandler(node, value, eventName) {
+    // 获取回调函数
+    const fn = this.$vm.$options.methods && this.$vm.$options.methods[value];
+    if(eventName && fn) {
+      node.addEventListener(eventName, fn.bind(this.$vm));
+    }
   }
 }
