221164d89a579b32c7bfe030c2b7dc6ab8463fe5
wangxing <wuyu1562766289@live.com>
2020-03-14 18:41:44 +0800

    网络

---
+++ node/net/http.js
@@ -0,0 +1,38 @@
+// 命令行查看
+// curl -v http://www.baidu.com
+
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+  const { method, url } = req;
+  console.log('method:', method);
+
+  console.log('cookie', req.headers.cookie);
+
+  if (method === 'GET' && url === '/') {
+    fs.readFile('./index.html', (err, data) => {
+      if (err) return;
+      res.setHeader("Content-Type", 'text/html');
+      res.end(data);
+    })
+  } else if ((method === 'GET' || method === 'POST') && url === '/users') {
+    res.setHeader('Content-Type', 'application/json');
+    setHeader(res);
+
+    res.setHeader('Set-Cookie', 'cookie1=haha;');
+    res.end(JSON.stringify([{ name: 'tom', age: 20 }]));
+  } else if (method == 'OPTIONS' && url == '/users') {
+    setHeader(res);
+    res.end();
+  }
+}).listen(3000);
+
+function setHeader(res) {
+  // 解决简单的跨域问题
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8080');
+  // 
+  res.setHeader('Access-Control-Allow-Headers', 'X-Token,Content-Type');
+  // 允许鉴权存在
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+}
